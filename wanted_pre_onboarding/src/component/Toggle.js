5b9d003a6f63b5733dbf458de88093548aaddfcb
h1un <h1unha@naver.com>
2022-02-12 03:46:31 +0900

    feat : Toggle.js

---
+++ wanted_pre_onboarding/src/component/Toggle.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import './../App.css';
+import './Toggle.css';
+
+const Toggle = () => {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const onToggle = () => {
+        setIsToggled(!isToggled);
+    };
+
+    const toggleChecked = isToggled ? 'ON' : 'OFF';
+
+    return (
+        <div className="wrapper ">
+            <h2>Toggle</h2>
+
+            <label className="switch">
+                <input type="checkbox" onClick={() => onToggle()}/>
+                <span className="slider"></span>
+            </label>
+            <p>Toggle Switch {toggleChecked}</p>
+        </div>
+    )
+};
+
+export default Toggle;
